fdd477488ac7a1a0ac114ad88be48a35c71e6d04
shijiezhang <shijiezhang@sohu-inc.com>
2020-12-29 10:20:51 +0800

    beat 1

---
+++ app/src/main/java/app/eeh/bank/TextActivity.java
@@ -0,0 +1,19 @@
+package app.eeh.bank;
+
+import android.os.Bundle;
+
+import com.erajie.base.BaseActivity;
+import com.erajie.rxutils.RxLogTool;
+
+/**
+ * @author EraJieZhang
+ * @data 2020-12-29
+ */
+public class TextActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RxLogTool.d("1");
+    }
+}
